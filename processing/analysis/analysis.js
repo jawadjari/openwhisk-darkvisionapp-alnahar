4b418fc65ba86c9218c97502455a6a419ca02a73
jawadjari <jawadjari@ma.ibm.com>
2017-11-06 18:52:15 +0000

    Update analysis.js

--- processing/analysis/analysis.js
+++ processing/analysis/analysis.js
@@ -240,7 +240,7 @@ function analyzeImage(args, fileName, analyzeCallback) {
           url: 'https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify' + // eslint-disable-line
             '?api_key=' + args.watsonApiKey +
             '&version=2016-05-20',
-          parameters: {'classifier_ids':'[\"default\"]'},
+          parameters: {'classifier_ids':'[\"media_416609574\"]'},
           headers: {
             'Content-Length': fs.statSync(fileName).size
           },
